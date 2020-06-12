c3dfa023f00401b8970f597a300b11b0a79d61a1
Diego Lopes <dihgg@dihgg.com>
2020-06-12 17:26:49 -0300

    titulo dinamico no header

--- ng/src/app/base/header/header.component.ts
+++ ng/src/app/base/header/header.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { WordpressService } from 'src/app/services/wordpress.service';
 import { MenuItem } from 'src/app/services/wordpress.interface';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-header',
@@ -12,7 +13,8 @@ export class HeaderComponent implements OnInit {
   public header: MenuItem[] = [];
 
   constructor(
-    public wordpress: WordpressService
+    public wordpress: WordpressService,
+    public title: Title
   ) { }
 
   ngOnInit(): void {
