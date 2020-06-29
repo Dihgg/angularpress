fd6b0a39d03ea5f46aeb22e19d1cf3bbcf5df18d
Diego Lopes <dihgg@dihgg.com>
2020-06-29 12:38:03 -0300

    header footer e title

--- ng/src/index.php
+++ ng/src/index.php
@@ -4,6 +4,8 @@
 <head>
   <meta charset="utf-8">
   <title><?php echo bloginfo('name') ?></title>
+  <?php wp_head(); ?>
+  <title><?php echo wp_title('|') ?></title>
   <base href="<?php echo home_url() ?>">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="icon" type="image/x-icon" href="favicon.ico">
@@ -14,6 +16,7 @@
 
 <body>
   <app-root></app-root>
+  <?php get_footer(); ?>
 </body>
 
 </html>
