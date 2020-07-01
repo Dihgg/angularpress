acce42ccf13b67e2251a956fcab8d6babae48b21
Diego Lopes <dihgg@dihgg.com>
2020-07-01 15:59:45 -0300

    criação de customizer
    Algumas configurações de estilo foram adicionadas ao customizer do wordpress, dando maior flexibilidade

--- ng/src/index.php
+++ ng/src/index.php
@@ -1,22 +1,37 @@
 <!doctype html>
-<html lang="en">
+<html <?php echo get_language_attributes() ?>>
 
 <head>
   <meta charset="utf-8">
-  <title><?php echo bloginfo('name') ?></title>
   <?php wp_head(); ?>
   <title><?php echo wp_title('|') ?></title>
   <base href="<?php echo home_url() ?>">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="icon" type="image/x-icon" href="favicon.ico">
-  <script type="text/javascript">
-    const BASE_HREF = `<?php echo home_url() ?>`;
-  </script>
 </head>
 
-<body>
+<body class="theme--bg theme--color">
   <app-root></app-root>
   <?php get_footer(); ?>
+  <?php require_once('theme.css.php'); ?>
+  <script type="text/javascript">
+    const BASE_HREF = `<?php echo home_url() ?>`;
+    const THEME = {
+      logos: {
+        mobile: `<?php echo get_theme_mod('mobile_logo_image'); ?>`,
+        desktop: `<?php echo get_theme_mod('desktop_logo_image'); ?>`
+      },
+      options: {
+        header: {
+          socials: <?php echo (get_theme_mod('header_show_socials', 'on') === 'on') ? 'true' : 'false'; ?>
+        },
+        footer: {
+          socials: <?php echo (get_theme_mod('footer_show_socials', 'off') === 'on') ? 'true' : 'false'; ?>
+        }
+      }
+    }
+  </script>
+
 </body>
 
 </html>
---
+++ ng/src/wordpress/customizer/customizer.php
@@ -0,0 +1,229 @@
+<?php
+
+add_action('customize_register', 'color_customizer');
+add_action('customize_register', 'logo_customizer');
+add_action('customize_register', 'options_customizer');
+
+function color_customizer($wp_customize)
+{
+	$wp_customize->add_section('theme_colors_settings', array(
+		'title' => __('Colors', 'angularpress'),
+		'priority' => 5,
+	));
+
+	$theme_colors = array(
+		// Background Color
+		array(
+			'slug' => 'background_color',
+			'default' => '#ffffff',
+			'label' => __('Background', 'angularpress')
+		),
+		// Menus Background
+		array(
+			'slug' => 'menu_background_color',
+			'default' => '#666666',
+			'label' => __('Menu Background', 'angularpress')
+		),
+		// Text Colors
+		array(
+			'slug' => 'text_color',
+			'default' => '#000000',
+			'label' => __('Text', 'angularpress')
+		),
+		// Menu Text color
+		array(
+			'slug' => 'menu_text_color',
+			'default' => '#ffffff',
+			'label' => __('Menu Color', 'angularpress')
+		)
+	);
+
+	foreach ($theme_colors as $color) {
+
+		$wp_customize->add_setting(
+			$color['slug'],
+			array(
+				'default' => $color['default'],
+				'sanitize_callback' => 'sanitize_hex_color',
+				'type' => 'theme_mod',
+				'capability' => 'edit_theme_options',
+				'transport' => 'refresh'
+			)
+		);
+
+		$wp_customize->add_control(
+			new WP_Customize_Color_Control(
+				$wp_customize,
+				$color['slug'],
+				array(
+					'label' => $color['label'],
+					'section' => 'theme_colors_settings',
+					'settings' => $color['slug']
+				)
+			)
+		);
+	}
+}
+
+function logo_customizer($wp_customize) {
+	$wp_customize->add_section('theme_logo_settings', array(
+		'title' => __('Logos', 'angularpress'),
+		'priority' => 5,
+	));
+	
+	$theme_logos = array(
+		// Mobile logo
+		array(
+			'slug' => 'mobile_logo_image',
+			'default' => '',
+			'label' => __('Mobile Logo', 'angularpress')
+		),
+		// Desktop logo
+		array(
+			'slug' => 'desktop_logo_image',
+			'default' => '',
+			'label' => __('Desktop Logo', 'angularpress')
+		)
+	);
+
+	foreach($theme_logos as $logo) {
+		
+		$wp_customize->add_setting(
+			$logo['slug'],
+			array(
+				'default' => $logo['default'],
+				'sanitize_callback' => 'angularpress_sanitize_file',
+				'type' => 'theme_mod',
+				'capability' => 'edit_theme_options',
+				'transport' => 'refresh'
+			)
+		);
+
+		$wp_customize->add_control(
+			new WP_Customize_Image_Control(
+				$wp_customize,
+				$logo['slug'],
+				array(
+					'label' => $logo['label'],
+					'section' => 'theme_logo_settings',
+					'settings' => $logo['slug']
+				)
+			)
+		);
+	}
+}
+
+function options_customizer($wp_customize) {
+
+	$sections = array(
+		'theme_header_settings' => array(
+			// Options for HEADER
+			'label' => __('Header', 'angularpress'),
+			'options' => array(
+				// Show socials
+				array(
+					'slug' => 'header_show_socials',
+					'default' => 'on',
+					'type'	=> 'radio',
+					'choices' => array(
+						'on' => __( 'On', 'angularpress' ),
+						'off' => __( 'Off', 'angularpress' )
+					),
+					'label' => __('Show Socials', 'angularpress'),
+					'description' => __( 'Show Socials menu.', 'angularpress' )
+				),
+			)
+		),
+		'theme_footer_settings' => array(
+			// Options for HEADER
+			'label' => __('Footer', 'angularpress'),
+			'options' => array(
+				// Show socials
+				array(
+					'slug' => 'footer_show_socials',
+					'default' => 'off',
+					'type'	=> 'radio',
+					'choices' => array(
+						'on' => __( 'On', 'angularpress' ),
+						'off' => __( 'Off', 'angularpress' )
+					),
+					'label' => __('Show Socials', 'angularpress'),
+					'description' => __( 'Show Socials menu.', 'angularpress' )
+				),
+			)
+		),
+	);
+	
+	foreach ($sections as $id => $section) {
+		$wp_customize->add_section($id, array(
+			'title' => $section['label'],
+			'priority' => 6,
+		));
+
+		foreach($section['options'] as $option) {
+		
+			$wp_customize->add_setting(
+				$option['slug'],
+				array(
+					'default' => $option['default'],
+					'sanitize_callback' => 'angularpress_sanitize_select',
+					'type' => 'theme_mod',
+					'capability' => 'edit_theme_options',
+					'transport' => 'refresh'
+				)
+			);
+
+			$wp_customize->add_control(
+				new WP_Customize_Control(
+					$wp_customize,
+					$option['slug'],
+					array(
+						'type' => 'radio',
+						'section' => $id,
+						'label' => $option['label'],
+						'description' => $option['description'],
+						'choices' => $option['choices'],
+					)
+				)
+			);
+	
+			/* $wp_customize->add_control( $option['slug'], array(
+				'type' => 'radio',
+				'section' => $id,
+				'label' => $option['label'],
+				'description' => $option['description'],
+				'choices' => $option['choices'],
+			) ); */
+		}
+	}
+}
+
+
+// SANITIZERS
+function angularpress_sanitize_file( $file, $setting ) {
+          
+	//allowed file types
+	$mimes = array(
+		'jpg|jpeg|jpe' => 'image/jpeg',
+		'gif'          => 'image/gif',
+		'png'          => 'image/png'
+	);
+	  
+	//check file type from file name
+	$file_ext = wp_check_filetype( $file, $mimes );
+	  
+	//if file has a valid mime type return it, otherwise return default
+	return ( $file_ext['ext'] ? $file : $setting->default );
+}
+
+function angularpress_sanitize_select( $input, $setting ) {
+
+	// Ensure input is a slug.
+	$input = sanitize_key( $input );
+  
+	// Get list of choices from the control associated with the setting.
+	$choices = $setting->manager->get_control( $setting->id )->choices;
+  
+	// If the input is a valid key, return it; otherwise, return the default.
+	return ( array_key_exists( $input, $choices ) ? $input : $setting->default );
+  }
