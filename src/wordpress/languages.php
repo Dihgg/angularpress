e5367148be1d40664f0e1c1168f9436aaa9287cc
Diego Lopes <dihgg@dihgg.com>
2020-08-21 17:46:53 -0300

    ajustes nas traduções

--- src/wordpress/languages.php
+++ src/wordpress/languages.php
@@ -8,9 +8,11 @@ function translations()
 				'Developed by'			=> __('Developed by', 'angularpress'),
 				'You searched by'		=> __('You searched by', 'angularpress'),
 				'No results found'		=> __('No results found', 'angularpress'),
-				'Results found'			=> __('Results found', 'angularpress'),
+				'Results found'			=> __('Results Found', 'angularpress'),
 				'Search'				=> __('Search', 'angularpress'),
 				'Load More'				=> __('Carregar Mais', 'angularpress'),
+				'Category'				=> __('Categoria', 'angularpress'),
+				'Tag'					=> __('Tag', 'angularpress'),
 			)
 		),
 		ENT_QUOTES,
